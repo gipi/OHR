331aa1aa8f363c7a39f98bdc4dd98216ba730140
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2015-09-30 18:36:50 +0200

    Fix factory.

--- OWR/oh/factory.py
+++ OWR/oh/factory.py
@@ -5,3 +5,6 @@ import factory
 class OpenHardwareFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = OpenHardware
+
+    name = factory.Sequence(lambda n: 'name {0}'.format(n))
+    slug = factory.Sequence(lambda n: 'slug-{0}'.format(n))
